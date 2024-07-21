bc5951873cce1b24946e9ebd1d071f4b04ce4c5b
VitaliyDolceVita <47143915+VitaliyDolceVita@users.noreply.github.com>
2024-07-21 15:09:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ import streamlit as st
 import pandas as pd
 import pickle
 
-# Завантаження збереженої моделі
+# Завантаження збереженої моделі та скалера
 with open('rf_model.pkl', 'rb') as file:
     rf_model = pickle.load(file)
 
+with open('scaler.pkl', 'rb') as file:
+    scaler = pickle.load(file)
 
 st.markdown("# :rainbow[Прогнозування Відтоку Клієнтів]")
 
@@ -17,18 +19,18 @@ def user_input_features():
     is_movie_package_subscriber = st.sidebar.selectbox("Чи є абонентом пакету фільмів?/is_movie_package_subscriber", [0, 1], key='is_movie_package_subscriber')
     subscription_age = st.sidebar.number_input("Термін підписки/subscription_age", min_value=0.0, max_value=100.0, step=0.01, key='subscription_age')
     bill_avg = st.sidebar.number_input("Середній рахунок/bill_avg", min_value=0.0, max_value=1000.0, step=0.01, key='bill_avg')
-    remaining_contract = st.sidebar.number_input("Залишок контракту/remaining_contract", min_value=0.0, max_value=100.0, step=0.01, key='reamining_contract')
+    reamining_contract = st.sidebar.number_input("Залишок контракту/remaining_contract", min_value=0.0, max_value=100.0, step=0.01, key='remaining_contract')
     service_failure_count = st.sidebar.number_input("Кількість збоїв у сервісі/service_failure_count", min_value=0, max_value=100, step=1, key='service_failure_count')
     download_avg = st.sidebar.number_input("Середня швидкість скачування/download_avg", min_value=0.0, max_value=100.0, step=0.01, key='download_avg')
     upload_avg = st.sidebar.number_input("Середня швидкість завантаження/upload_avg", min_value=0.0, max_value=100.0, step=0.01, key='upload_avg')
-    download_over_limit = st.sidebar.number_input("Кількість перевищеннь ліміту скачування?/download_over_limit", min_value=0, max_value=100, step=1, key='download_over_limit')
+    download_over_limit = st.sidebar.number_input("Кількість перевищень ліміту скачування?/download_over_limit", min_value=0, max_value=100, step=1, key='download_over_limit')
 
     data = {
         'is_tv_subscriber': is_tv_subscriber,
         'is_movie_package_subscriber': is_movie_package_subscriber,
         'subscription_age': subscription_age,
         'bill_avg': bill_avg,
-        'reamining_contract': remaining_contract,
+        'reamining_contract': reamining_contract,
         'service_failure_count': service_failure_count,
         'download_avg': download_avg,
         'upload_avg': upload_avg,
@@ -40,9 +42,9 @@ def user_input_features():
 
 input_df = user_input_features()
 
-if input_df is not None: # гарантує, що дані для обробки були введені користувачем
-    def preprocess_input(df): # Попередня обробка даних
-        df = df.astype(float)
+if input_df is not None:  # гарантує, що дані для обробки були введені користувачем
+    def preprocess_input(df):  # Попередня обробка даних
+        df = scaler.transform(df)
         return df
 
     preprocessed_input = preprocess_input(input_df)
@@ -64,3 +66,5 @@ if input_df is not None: # гарантує, що дані для обробки
     else:
         st.markdown("<h2 style='font-size:28px; color: green;'>Клієнт має низьку ймовірність відтоку</h2>",
                     unsafe_allow_html=True)
+
+
